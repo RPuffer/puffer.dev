c45557c82e5f17c2f058b5ba4fb7d8f300d5b23a
Ryne Puffer <rynepuffer@northwesternmutual.com>
2019-03-06 14:43:00 -0600

    try favicon thing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import favicon from './favicon.ico'
+
+const link = document.createElement('link');
+link.rel = 'shortcut icon';
+link.href = favicon;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
