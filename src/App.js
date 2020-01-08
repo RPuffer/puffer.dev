9386913f5698c4e10ee41693424d518fb3af471e
Ryne Puffer <rynepuffer@northwesternmutual.com>
2020-01-08 16:11:28 -0600

    feat: sail

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import base from './base.png';
 import logo from './rotate-logo.png';
+import gh from './GitHub-Mark-Light.png';
+import home from './home.png';
+import sail from './sail.png';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import './App.css';
+import SailBtn from './Sail';
+
+const GH = styled.a`
+  position: absolute;
+  left: 10px;
+  top: 10px;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+`
+const Home = styled.a`
+  position: absolute;
+  left: 10px;
+  top: 52px;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+`
+const Sail = styled.a`
+  position: absolute;
+  left: 10px;
+  top: 89px;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="App-logo-container">
-            <img src={base} className="App-logo-base" alt="base" />
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <p>
-            Under construction.
-          </p>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <div className="App-logo-container">
+                    <img src={base} className="App-logo-base" alt="base" />
+                    <img src={logo} className="App-logo" alt="logo" />
+                  </div>
+                  <p>
+                    Under construction.
+                  </p>
+              </Route>
+              <Route path="/sail">
+                <SailBtn />
+              </Route>
+            </Switch>
+          </Router>
+          <GH href="https://github.com/RPuffer/puffer.dev">
+            <img src={gh} alt="github source"/>
+          </GH>
+          <Home href="/">
+            <img src={home} alt="github source"/>
+          </Home>
+          <Sail href="/sail">
+            <img src={sail} alt="github source"/>
+          </Sail>
         </header>
       </div>
     );
---
+++ src/Sail/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components'
+import sail from './sail.mp3'
+import btn from './btn.png'
+import btnHover from './btn-hover.png'
+// import btnClick from './btn-click.png'
+import btnClick from './btn-click-2.png'
+
+const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
+	background: ${p => p.bg};
+`;
+
+const FancyBtn = styled.img`
+	border: 10px solid rgba(255,255,255,${p => p.opacity});
+	border-radius: 10px;
+
+	width: 200px;
+`
+
+
+class Sail extends React.Component {
+	state = {
+		mousein: false,
+		button: btn,
+		audio: new Audio(sail),
+		opacity: '0.1'
+	}
+
+	handleMouseLeave = () => {
+		this.setState({ button: btn, mousein: false, opacity: '0.1' })
+	}
+	handleMouseEnter = () => {
+		this.setState({ button: btnHover, mousein: true, opacity: '0.4' })
+	}
+	handleMouseDown = () => {
+		this.state.audio.play()
+		this.setState({ button: btnClick })
+	}
+	handleMouseUp = () => {
+		const { mousein } = this.state;
+		this.setState({ button: mousein ? btnHover : btn })
+	}
+
+	render() {
+		return (
+		  <Container>
+			<FancyBtn opacity={this.state.opacity} src={this.state.button} alt="imabutton"
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+				onMouseDown={this.handleMouseDown}
+				onMouseUp={this.handleMouseUp}
+			/>
+		  </Container>
+		);
+	}
+}
+
+export default Sail;
